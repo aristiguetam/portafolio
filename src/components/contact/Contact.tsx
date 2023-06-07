e343f9a8bd690b4dcc51099d5d9515d7f0605612
aristiguetam <aristiguetam97@gmail.com>
2023-06-07 18:45:40 -0500

    arreglando imagen

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -7,7 +7,7 @@ export const Contact = () => {
                 <h2 className="dark:text-[#2B2C34] text-[#FFFFFE] text-3xl font-bold sm:text-7xl sm:font-extrabold text-center">
                     Trabajemos <span className="text-[#7f5af0]">juntos.</span>
                 </h2>
-                <p className="text-sm font-medium sm:text-lg text-ellipsis text-center dark:text-[#2B2C34] text-[#FFFFFE] mt-2 sm:font-semibold">¿Tienes una idea genial que quieras llevar a cabo? Contáctame y hagamos realidad tu proyecto.</p>
+                <p className="px-4 md:px-0 text-sm font-medium sm:text-lg text-ellipsis text-center dark:text-[#2B2C34] text-[#FFFFFE] mt-2 sm:font-semibold">¿Tienes una idea genial que quieras llevar a cabo? Contáctame y hagamos realidad tu proyecto.</p>
             </div>
 
             <div className="w-[350px]  h-[250px]  sm:w-[800px] sm:h-[300px] my-7 ">
