033e410ef8087ed740d63660be33f2b01d7ac46c
aristiguetam <aristiguetam97@gmail.com>
2023-06-07 19:46:52 -0500

    arreglando submit contact

--- src/sendEmail/sendEmail.ts
+++ src/sendEmail/sendEmail.ts
@@ -6,9 +6,10 @@ interface Props {
     title: string
 }
 
+const host = process.env.HOSTNAME || "http://localhost:3000"
 export const sendEmail = async ({ asunto, email, title }: Props) => {
     try {
-        const { data } = await axios.post(`${process.env.HOSTNAME}/api/sendEmail`, {  title, email, asunto })
+        const { data } = await axios.post(`${host}/api/sendEmail`, {  title, email, asunto })
         return data 
     } catch (error) {
         console.error('Error al enviar el correo electrónico:', error);
