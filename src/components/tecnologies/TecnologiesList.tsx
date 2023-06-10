f0f638b5a5910a0ee5a3b4468b518aae6e115fed
aristiguetam <aristiguetam97@gmail.com>
2023-06-09 20:42:27 -0500

    modificando el carrusel

--- src/components/tecnologies/TecnologiesList.tsx
+++ src/components/tecnologies/TecnologiesList.tsx
@@ -1,87 +1,26 @@
-// import Image from "next/image"
-
-// export const TecnologiesList = () => {
-
-//     return (
-//         <div className='bg-transparent relative flex items-center overflow-hidden '>
-//             <div className='mb-12 sm:my-4 flex w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth gap-3 scrollbar-hide '>
-                // <Image width={0} height={0} sizes="100vw" src={'/01.svg'} alt={'js'} className="inline-block h-[60px] sm:h-[120px] w-[100%] rounded-lg object-cover justify-center items-center" />
-                // <Image width={0} height={0} sizes="100vw" src={'/02.svg'} alt={'node'} className="inline-block h-[60px] sm:h-[120px] w-[100%] rounded-lg object-cover justify-center items-center" />
-                // <Image width={0} height={0} sizes="100vw" src={'/03.svg'} alt={'postgress'} className="inline-block h-[60px] sm:h-[120px] w-[100%] rounded-lg object-cover justify-center items-center" />
-                // <Image width={0} height={0} sizes="100vw" src={'/04.svg'} alt={'tp'} className="inline-block h-[60px] sm:h-[120px] w-[100%] rounded-lg object-cover justify-center items-center" />
-                // <Image width={0} height={0} sizes="100vw" src={'/05.svg'} alt={'js'} className="inline-block h-[60px] sm:h-[120px] w-[100%] rounded-lg object-cover justify-center items-center" />
-                // <Image width={0} height={0} sizes="100vw" src={'/06.svg'} alt={'mongo'} className="inline-block h-[60px] sm:h-[120px] w-[100%] rounded-lg object-cover justify-center items-center" />
-                // <Image width={0} height={0} sizes="100vw" src={'/07.svg'} alt={'postman'} className="inline-block h-[60px] sm:h-[120px] w-[100%] rounded-lg object-cover justify-center items-center" />
-                // <Image width={0} height={0} sizes="100vw" src={'/08.svg'} alt={'react'} className="inline-block h-[60px] sm:h-[120px] w-[100%] rounded-lg object-cover justify-center items-center" />
-                // <Image width={0} height={0} sizes="100vw" src={'/09.svg'} alt={'react'} className="inline-block h-[60px] sm:h-[120px] w-[100%] rounded-lg object-cover justify-center items-center" />
-                // <Image width={0} height={0} sizes="100vw" src={'/10.svg'} alt={'react'} className="inline-block h-[60px] sm:h-[120px] w-[100%] rounded-lg object-cover justify-center items-center" />
-                // <Image width={0} height={0} sizes="100vw" src={'/11.svg'} alt={'react'} className="inline-block h-[60px] sm:h-[120px] w-[100%] rounded-lg object-cover justify-center items-center" />
-                // <Image width={0} height={0} sizes="100vw" src={'/12.svg'} alt={'react'} className="inline-block h-[60px] sm:h-[120px] w-[100%] rounded-lg object-cover justify-center items-center" />
-                // <Image width={0} height={0} sizes="100vw" src={'/13.svg'} alt={'react'} className="inline-block h-[60px] sm:h-[120px] w-[100%] rounded-lg object-cover justify-center items-center" />
-                // <Image width={0} height={0} sizes="100vw" src={'/14.svg'} alt={'react'} className="inline-block h-[60px] sm:h-[120px] w-[100%] rounded-lg object-cover justify-center items-center" />
-
-//             </div>
-//         </div>
-//     )
-// }
-
-import Image from "next/image";
+import Image from "next/image"
 
 export const TecnologiesList = () => {
-  return (
-    <div className="bg-transparent relative flex items-center overflow-hidden">
-      <div className="mb-12 sm:my-4 flex w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth gap-3 scrollbar-hide">
-        <Image
-          width={120}
-          height={120}
-          src="/01.svg"
-          alt="js"
-          className="inline-block h-60 sm:h-120 w-full rounded-lg object-cover justify-center items-center"
-        />
-         <Image
-          width={120}
-          height={120}
-          src="/01.svg"
-          alt="js"
-          className="inline-block h-60 sm:h-120 w-full rounded-lg object-cover justify-center items-center"
-        />
-         <Image
-          width={120}
-          height={120}
-          src="/01.svg"
-          alt="js"
-          className="inline-block h-60 sm:h-120 w-full rounded-lg object-cover justify-center items-center"
-        />
-         <Image
-          width={120}
-          height={120}
-          src="/01.svg"
-          alt="js"
-          className="inline-block h-60 sm:h-120 w-full rounded-lg object-cover justify-center items-center"
-        />
-         <Image
-          width={120}
-          height={120}
-          src="/01.svg"
-          alt="js"
-          className="inline-block h-60 sm:h-120 w-full rounded-lg object-cover justify-center items-center"
-        />
-         <Image
-          width={120}
-          height={120}
-          src="/01.svg"
-          alt="js"
-          className="inline-block h-60 sm:h-120 w-full rounded-lg object-cover justify-center items-center"
-        />
-         <Image
-          width={120}
-          height={120}
-          src="/01.svg"
-          alt="js"
-          className="inline-block h-60 sm:h-120 w-full rounded-lg object-cover justify-center items-center"
-        />
-        
-      </div>
-    </div>
-  );
-};
+
+    return (
+        <div className='bg-transparent relative flex items-center overflow-hidden '>
+            <div className='mb-12 sm:my-4 flex w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth gap-3 scrollbar-hide '>
+                <Image width={0} height={0} sizes="100vw" src={'/01.svg'} alt={'js'} className="inline-block h-[60px] sm:h-[120px] w-[100%] rounded-lg object-cover justify-center items-center" />
+                <Image width={0} height={0} sizes="100vw" src={'/02.svg'} alt={'node'} className="inline-block h-[60px] sm:h-[120px] w-[100%] rounded-lg object-cover justify-center items-center" />
+                <Image width={0} height={0} sizes="100vw" src={'/03.svg'} alt={'postgress'} className="inline-block h-[60px] sm:h-[120px] w-[100%] rounded-lg object-cover justify-center items-center" />
+                <Image width={0} height={0} sizes="100vw" src={'/04.svg'} alt={'tp'} className="inline-block h-[60px] sm:h-[120px] w-[100%] rounded-lg object-cover justify-center items-center" />
+                <Image width={0} height={0} sizes="100vw" src={'/05.svg'} alt={'js'} className="inline-block h-[60px] sm:h-[120px] w-[100%] rounded-lg object-cover justify-center items-center" />
+                <Image width={0} height={0} sizes="100vw" src={'/06.svg'} alt={'mongo'} className="inline-block h-[60px] sm:h-[120px] w-[100%] rounded-lg object-cover justify-center items-center" />
+                <Image width={0} height={0} sizes="100vw" src={'/07.svg'} alt={'postman'} className="inline-block h-[60px] sm:h-[120px] w-[100%] rounded-lg object-cover justify-center items-center" />
+                <Image width={0} height={0} sizes="100vw" src={'/08.svg'} alt={'react'} className="inline-block h-[60px] sm:h-[120px] w-[100%] rounded-lg object-cover justify-center items-center" />
+                <Image width={0} height={0} sizes="100vw" src={'/09.svg'} alt={'react'} className="inline-block h-[60px] sm:h-[120px] w-[100%] rounded-lg object-cover justify-center items-center" />
+                <Image width={0} height={0} sizes="100vw" src={'/10.svg'} alt={'react'} className="inline-block h-[60px] sm:h-[120px] w-[100%] rounded-lg object-cover justify-center items-center" />
+                <Image width={0} height={0} sizes="100vw" src={'/11.svg'} alt={'react'} className="inline-block h-[60px] sm:h-[120px] w-[100%] rounded-lg object-cover justify-center items-center" />
+                <Image width={0} height={0} sizes="100vw" src={'/12.svg'} alt={'react'} className="inline-block h-[60px] sm:h-[120px] w-[100%] rounded-lg object-cover justify-center items-center" />
+                <Image width={0} height={0} sizes="100vw" src={'/13.svg'} alt={'react'} className="inline-block h-[60px] sm:h-[120px] w-[100%] rounded-lg object-cover justify-center items-center" />
+                <Image width={0} height={0} sizes="100vw" src={'/14.svg'} alt={'react'} className="inline-block h-[60px] sm:h-[120px] w-[100%] rounded-lg object-cover justify-center items-center" />
+
+            </div>
+        </div>
+    )
+}
